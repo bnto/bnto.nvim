d51f6c4cc107e1b10f02a34ff7ce4bb7431ebd5a
bnto <bnto@users.noreply.github.com>
2024-04-30 17:32:40 +0200

    Add telescope and which-key nvim plugins

---
+++ lua/bnto/plugins/telescope.lua
@@ -0,0 +1,35 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+          },
+        },
+      },
+    })
+
+    telescope.load_extension("fzf")
+
+    -- set keymaps
+    vim.keymap.set("n", "<leader>f", "<CMD>Telescope find_files<CR>", { desc = "Find files in cwd" })
+
+    -- other commands to consider keymapping:
+    -- "Telescope oldfiles" - Find recent files
+    -- "Telescope live_grep" - Find string in cwd
+    -- "Telescope grep_string" - Find string under cursor in cwd
+  end,
+}
