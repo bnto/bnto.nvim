62c9cacb34ae3438b4571c93a04f4f762f45283d
bnto <bnto@users.noreply.github.com>
2024-04-30 15:53:44 +0200

    Add plenary and nvim-tree to my nvim plugin list

---
+++ lua/bnto/plugins/nvim-tree.lua
@@ -0,0 +1,26 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    local nvimtree = require("nvim-tree")
+
+    -- bypass default file explorer
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    nvimtree.setup({
+      filters = {
+        custom = { ".DS_Store" },
+      },
+      git = {
+        ignore = false
+      },
+    })
+
+    -- set keymaps
+    local keymap = vim.keymap
+
+    keymap.set("n", "<leader>b", "<CMD>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer" })
+  end
+
+}
