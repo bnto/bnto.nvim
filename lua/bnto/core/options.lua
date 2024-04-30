65a1ba480e2fda04a830949414acaa1fd1c4bf53
bnto <bnto@users.noreply.github.com>
2024-04-30 09:06:21 +0200

    Set up initial vim options

---
+++ lua/bnto/core/options.lua
@@ -0,0 +1,37 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & identation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- line settings
+opt.cursorline = true
+opt.scrolloff = 10
+-- opt.wrap = false
+
+-- termguicolors
+opt.termguicolors = true
+-- opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard
+
+-- split windows
+opt.splitright = true
+opt.splitbelow = true
+
