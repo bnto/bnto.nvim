86da80adf1e1f7ce5519c9b6ccea1fb8d86fcdc8
bnto <bnto@users.noreply.github.com>
2024-05-01 15:31:09 +0200

    Set up formatting and linting for nvim

---
+++ lua/bnto/plugins/formatting.lua
@@ -0,0 +1,32 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+        json = { "prettier" },
+        markdown = { "prettier" },
+        yaml = { "prettier" },
+        lua = { "stylua" },
+      },
+      -- format_on_save = {
+      --   lsp_fallback = true,
+      --   sync = false,
+      --   timeout_ms = 1000,
+      -- },
+    })
+
+    local keymap = vim.keymap
+    keymap.set({ "n", "v" }, "<leader><leader>f", function()
+      conform.format({
+        lsp_fallback = true,
+        sync = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or visual selection" })
+  end,
+}
--- lua/bnto/plugins/lsp/mason.lua
+++ lua/bnto/plugins/lsp/mason.lua
@@ -2,15 +2,14 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     local mason = require("mason")
-
     local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
 
-    mason.setup({
-      
-    })
+    mason.setup({})
 
     mason_lspconfig.setup({
       -- list of servers to force install
@@ -24,5 +23,12 @@ return {
       },
     })
 
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier",
+        "stylua",
+        "eslint_d",
+      }
+    })
   end,
 }
