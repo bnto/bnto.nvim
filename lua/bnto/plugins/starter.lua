8093fea1b16d3f8d5f96b158b6e54a2b68223af5
bnto <bnto@users.noreply.github.com>
2024-06-11 11:58:13 +0200

    Disable starter, substitute and which-key plugin

--- lua/bnto/plugins/starter.lua
+++ lua/bnto/plugins/starter.lua
@@ -1,10 +1,10 @@
 return {
-  "echasnovski/mini.starter",
-  version = "*",
-  config = function()
-    local starter = require("mini.starter")
-    starter.setup({
-      -- config here
-    })
-  end
+  -- "echasnovski/mini.starter",
+  -- version = "*",
+  -- config = function()
+  --   local starter = require("mini.starter")
+  --   starter.setup({
+  --     -- config here
+  --   })
+  -- end
 } 
