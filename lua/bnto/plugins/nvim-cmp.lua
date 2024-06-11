1d3e8822930a54bc8c8ae291f7969bda1d4a2895
bnto <bnto@users.noreply.github.com>
2024-06-11 12:07:49 +0200

    Add luasnip to the cmp

--- lua/bnto/plugins/nvim-cmp.lua
+++ lua/bnto/plugins/nvim-cmp.lua
@@ -4,26 +4,43 @@ return {
   dependencies = {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
-    -- "saadparwaiz1/cmp_luasnip",
+    {
+      "L3MON4D3/Luasnip",
+      version = "v2.*",
+      build = "make install_jsregexp",
+    },
+    "saadparwaiz1/cmp_luasnip",
     "onsails/lspkind.nvim",
   },
   config = function()
     local cmp = require("cmp")
+    local luasnip = require("luasnip")
     local lspkind = require("lspkind")
+
+    require("luasnip.loaders.from_vscode").lazy_load()
 
     cmp.setup({
       completion = {
         completeopt = "menu,menuone,preview,noselect",
       },
+      snippet = { -- configure how nvim-cmp interacts with snippet engine
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
       mapping = cmp.mapping.preset.insert({
         ["<C-Space>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm({ select = false })
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["C-j"] = cmp.mapping.scroll_docs(4),
+        ["C-k"] = cmp.mapping.scroll_docs(-4),
       }),
 
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
+        { name = "luasnip" },
         { name = "buffer" },
         { name = "path" },
+        -- { name = "codeium" },
       }),
 
       formatting = {
