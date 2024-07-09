9fc0c104e4a458ab85f340fdc7ab00670c6d9053
bnto <bnto@users.noreply.github.com>
2024-07-09 08:22:22 +0200

    Update nvim keymaps

--- lua/bnto/core/keymaps.lua
+++ lua/bnto/core/keymaps.lua
@@ -10,11 +10,6 @@ keymap.set("n", "<ESC>", "<CMD>nohlsearch<CR>")
 -- window management
 keymap.set("n", "<leader>o", "<C-w>o", { desc = "Close other windows" })
 
--- NOTE: default keymaps for split :
--- <C-w>v : split vertically
--- <C-w>s : split horizontally
--- <CMD>close<CR> : close split
-
 -- keymap.set("n", "<leader>6", ":ls<CR>:b<Space>")
 keymap.set("n", "<leader>6", "<C-6>")
 
@@ -32,22 +27,29 @@ keymap.set("n", "<Down>", "<Nop>")
 keymap.set("n", "<Left>", "<Nop>")
 keymap.set("n", "<Right>", "<Nop>")
 
--- NOTE: Some interesting keymaps:
 -- Reselect visual selection after indenting.
 keymap.set("v", "<", "<gv")
 keymap.set("v", ">", ">gv")
+-- Move text up and down
+keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- Easy insertion of a trailing ; or , from insert mode.
-keymap.set('i', ';;', '<Esc>A;<Esc>')
-keymap.set('i', ',,', '<Esc>A,<Esc>')
+keymap.set("i", ";;", "<Esc>A;<Esc>")
+keymap.set("i", ",,", "<Esc>A,<Esc>")
 
 -- page jumps
 keymap.set("n", "<C-j>", "<C-d>zz")
 keymap.set("n", "<C-k>", "<C-u>zz")
 
+-- copy to clipboard
+keymap.set("n", "<leader>y", "\"+y")
+keymap.set("v", "<leader>y", "\"+y")
+keymap.set("n", "<leader>Y", "\"+Y")
+
 -- save & quit
 keymap.set("n", "<leader>q", "<CMD>q<CR>", { desc = "Quit current" })
 keymap.set("n", "<leader>s", "<CMD>w<CR>", { desc = "Save Document" })
-keymap.set("n", "<leader>Q", "<CMD>q!<CR>", { desc = "Force quit"})
+keymap.set("n", "<leader>Q", "<CMD>q!<CR>", { desc = "Force quit" })
 
 -- relaod config
-keymap.set("n", "<leader><leader><CR>", "<CMD>so $MYVIMRC<CR>")
+-- keymap.set("n", "<leader><leader><CR>", "<CMD>so $MYVIMRC<CR>")
