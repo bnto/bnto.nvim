c56ff6cc3fbc2a4adf7e391315115d170f49fc7e
bnto <bnto@users.noreply.github.com>
2024-07-24 10:28:37 +0200

    Use rose-pine-dawn colorscheme

--- lua/bnto/plugins/colorscheme.lua
+++ lua/bnto/plugins/colorscheme.lua
@@ -1,24 +1,14 @@
 return {
-
-  -- set tokyonight colorscheme
-  "folke/tokyonight.nvim",
-  priority = 1000,
+  "rose-pine/neovim",
+  name = "rose-pine",
   config = function()
-    require("tokyonight").setup({
-      style = "storm",
-      on_colors = function(colors)
-        -- merge background with wezterm bg
-        colors.bg = "#0B283A"
-        colors.bg_sidebar = "#0B283A"
-        colors.bg_highlight = "#0B283A"
-        colors.bg_float = "#0B283A"
-        -- Uncomment following for transparent backgrounds
-        -- colors.bg = ""
-        -- colors.bg_sidebar = ""
-        -- colors.bg_float = ""
-      end,
+    require("rose-pine").setup({
+      variant = "dawn",
+      -- dim_inactive_windows = true,
+      styles = {
+        transparency = true,
+      },
     })
-
-    vim.cmd("colorscheme tokyonight")
+    vim.cmd("colorscheme rose-pine-dawn")
   end,
 }
