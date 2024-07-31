32d9ffdabba8935a5ed0b0c8b7a4ef942986f671
bnto <bnto@users.noreply.github.com>
2024-07-31 10:24:29 +0200

    Use dark colorscheme

--- lua/bnto/plugins/colorscheme.lua
+++ lua/bnto/plugins/colorscheme.lua
@@ -3,12 +3,14 @@ return {
   name = "rose-pine",
   config = function()
     require("rose-pine").setup({
-      variant = "dawn",
+      -- variant = "dawn",
+      variant = "moon",
       -- dim_inactive_windows = true,
       styles = {
         transparency = true,
       },
     })
-    vim.cmd("colorscheme rose-pine-dawn")
+    -- vim.cmd("colorscheme rose-pine-dawn")
+    vim.cmd("colorscheme rose-pine-moon")
   end,
 }
