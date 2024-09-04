d0ea3cc460fc8bcd24e3e8523e0fea4c69234bcd
bnto <bnto@users.noreply.github.com>
2024-09-04 16:47:48 +0200

    Remove unused colorscheme variant

--- lua/bnto/plugins/colorscheme.lua
+++ lua/bnto/plugins/colorscheme.lua
@@ -3,7 +3,6 @@ return {
   name = "rose-pine",
   config = function()
     require("rose-pine").setup({
-      variant = "moon",
       dim_inactive_windows = false,
       styles = {
         transparency = true,
