cc3771dbcbc8709d1d2ff3ab462bca6425568b83
bnto <bnto@users.noreply.github.com>
2024-09-06 14:39:06 +0200

    Add gitsigns stage/unstage hunks keymap

--- lua/bnto/plugins/gitsigns.lua
+++ lua/bnto/plugins/gitsigns.lua
@@ -1,4 +1,20 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, desc)
+        vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+      end
+
+      -- git stage / unstage hunk
+      map("v", "<leader>gs", function()
+        gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+      end, "")
+      map("n", "<leader>gu", gs.undo_stage_hunk)
+
+    end,
+  },
 }
