0d6e4a64548889d8604d5c5a84c26d09bcef873c
bnto <bnto@users.noreply.github.com>
2024-09-09 08:04:15 +0200

    Fix missing border on the cmp panel

--- lua/bnto/plugins/colorscheme.lua
+++ lua/bnto/plugins/colorscheme.lua
@@ -17,5 +17,9 @@ return {
     vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
       border = "rounded",
     })
+    vim.lsp.handlers["textDocument/hover"] =
+      vim.lsp.with(vim.lsp.handlers.hover, {
+        border = "rounded",
+      })
   end,
 }
