4b6e16c9235d946547c8c3ebfd1babd3354bcbdd
bnto <bnto@users.noreply.github.com>
2025-01-23 10:59:30 +0100

    Make navigating the file tree more easy by using the space bar

--- lua/bnto/plugins/nvim-tree.lua
+++ lua/bnto/plugins/nvim-tree.lua
@@ -8,7 +8,29 @@ return {
     -- vim.g.loaded_netrw = 1
     -- vim.g.loaded_netrwPlugin = 1
 
+    local function my_on_attach(bufnr)
+      local api = require("nvim-tree.api")
+
+      local function opts(desc)
+        return {
+          desc = desc,
+          buffer = bufnr,
+          noremap = true,
+          silent = true,
+          nowait = true,
+        }
+      end
+
+      -- default mappings
+      api.config.mappings.default_on_attach(bufnr)
+
+      -- custom mappings
+      vim.keymap.set("n", "<space>", api.node.open.edit, opts("") )
+      vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("") )
+    end
+
     nvimtree.setup({
+      on_attach = my_on_attach,
       filters = {
         custom = { ".DS_Store", "node_modules" },
         dotfiles = false,
